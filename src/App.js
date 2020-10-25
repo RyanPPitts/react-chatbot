c22b3c20d275f0e40e416c10779b75b717bb86bc
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-10-24 19:10:07 -0700

    Chatbot quickstart installed.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Chatbot from "react-chatbot-kit";
+
+import config from "./config";
+import MessageParser from "./MessageParser";
+import ActionProvider from "./ActionProvider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       Hello World
-      </header>
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
     </div>
   );
 }
