bd2a549aed11c587b4f27766f9d30614829727f1
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-10-24 19:25:27 -0700

    BotAvatar Config/CSS
    Adding botAvatar to config and CSS

---
+++ src/components/BotAvatar/BotAvatar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './BotAvatar.css'
+
+const BotAvatar = () => {
+    return (
+        <div className="service-bot-avatar">
+            Service Bot
+            
+        </div>
+    )
+}
+
+export default BotAvatar
--- src/config.js
+++ src/config.js
@@ -1,8 +1,14 @@
 // Config starter code
+import React from 'react';
 import { createChatBotMessage } from "react-chatbot-kit";
+import BotAvatar from "./components/BotAvatar/BotAvatar";
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)]
+  initialMessages: [createChatBotMessage(`Hi, I'm your chatbot`)],
+  botName: "ServiceBot",
+  customComponents: {
+      botAvatar: (props) => <BotAvatar {...props} />
+  }
 }
 
 export default config
