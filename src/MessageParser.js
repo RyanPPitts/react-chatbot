37f316d39b5ed936b0ad9d86b0600c9667bcc2e8
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-10-26 16:38:15 -0700

    MessageParser
    Updating the logic in the message parser

--- src/MessageParser.js
+++ src/MessageParser.js
@@ -1,13 +1,24 @@
 // MessageParser starter code
+// when user writes information that informaiton is carried to the message parser
 class MessageParser {
     constructor(actionProvider, state) {
       this.actionProvider = actionProvider;
+      // inside this message parser we have access to the action provider
       this.state = state;
+      // state carried to action provider
     }
-  
+  // takes in the message and console logs the message typed by the user
+
     parse(message) {
       console.log(message)
+
+      const lowercase= message.toLowerCase()
+
+      if (lowercase.includes('hello')) {
+        console.log('hi')
+      }
     }
   }
   
   export default MessageParser;
+
