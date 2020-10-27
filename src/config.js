298eed4959ad88339ab731fd74a2a2a7b93a464c
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-10-26 17:02:32 -0700

    Creating widget
    Creating widget within config file to pull list of todo items

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { createChatBotMessage } from "react-chatbot-kit";
 import BotAvatar from "./components/BotAvatar/BotAvatar";
+import Todos from './components/Todos/Todos.jsx';
 
 const config = {
   initialMessages: [createChatBotMessage(`Hi, I'm your chatbot`)],
@@ -22,8 +23,17 @@ const config = {
 
   //define the state of the app
   state: { 
-      movieTitles: ["The lord of the rings", "Con air"]
-  }
+     todos: []
+  }, 
+  widgets: [
+      {
+       widgetName: "todos",
+       //function that returns JSX element
+       widgetFunc: (props) => <Todos {...props} />,
+       // mapstatetoprops - tells chatbot which state to give to this custom component
+       mapStateToProps: ['todos'], 
+      }
+  ]
 }
 
 export default config
